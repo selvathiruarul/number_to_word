2b13ca90c21c3cd3b4252c75e5bea60f20176d96
Selva <selvak26@gmail.com>
2017-09-26 18:45:08 -0500

    Add comments

--- number_to_word/main.py
+++ number_to_word/main.py
@@ -5,6 +5,8 @@ Created on Thu Oct 20 18:31:10 2016
 
 @author: selva
 """
+
+# Exhaustive list of words
 WORDS = {'-': 'negative', 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven',
          8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen',
          14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty',
