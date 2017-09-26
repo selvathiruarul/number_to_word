c9874dd7d8c6633433eaf17f8ff97244721c19e8
Selva <selvak26@gmail.com>
2017-09-26 18:31:21 -0500

    Fix name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
-setup(name='number_to_world', version='1.0.0',
-	  url='https://github.com/selvathiruarul/number_to_world', author='selva')
+setup(name='number_to_word', version='1.0.0',
+	  url='https://github.com/selvathiruarul/number_to_word', author='selva')
